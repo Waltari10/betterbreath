e28e5400e18d801a6e6779071014fbea2f66086d
valtteri laine <valtteri@veristable.com>
2024-07-29 09:19:32 +0300

    fix: add some placeholder logic for haptic feedback

--- BetterBreath/screens/BreathExerciseView.swift
+++ BetterBreath/screens/BreathExerciseView.swift
@@ -13,22 +13,65 @@ struct BreathExerciseView: View {
     @State private var audioPlayer: AVAudioPlayer?
     @State private var isActive = true
     @State private var timeElapsed = 0.0
+    @State private var vibrate = false
     
     
     var breathExercise: BreathExercise
     
     @State private var scale: CGFloat = 0.0 // Start small
     
+    private func queuVibrations(totalDuration: Double ) {
+        if !vibrate { return }
+        print("QueueVibrations")
+        
+        let numIntervals = 10
+        
+        let interval = totalDuration / Double(numIntervals - 1)
+        for i in 0..<numIntervals {
+            let easedTime = InOutQuadBlend(t: Double(i) * interval / totalDuration) * totalDuration
+            DispatchQueue.main.asyncAfter(deadline: .now() + easedTime) {
+                self.playChime()
+            }
+        }
+    }
+    
+    private func bezierBlend(t: Double) -> Double {
+        return t * t * (3 - 2 * t);
+    }
+    
+    private func InOutQuadBlend(t: Double) -> Double {
+        
+        if(t <= 0.5) {
+            return 2 * t * t
+        }
+        
+        let modT = t - 0.5;
+        return 2 * modT * (1 - modT) + 0.5;
+    }
+    
+    
+    // Double check is correct
+    private func easeInOut(t: Double) -> Double {
+        // This is a simple easeInOut function using a cubic polynomial expression
+        return t < 0.5 ? 4 * t * t * t : 1 - pow(-2 * t + 2, 3) / 2
+    }
+    
     private func animateCircle() {
         if !isActive { return }
         
         
+        
         withAnimation(.easeInOut(duration: breathExercise.inBreathDuration)) {
+            
+            queuVibrations(totalDuration: breathExercise.inBreathDuration)
             scale = 1.0
         }
         
-        withAnimation(.easeInOut(duration: breathExercise.outBreathDuration).delay(breathExercise.inBreathDuration + breathExercise.fullBreathHoldDuration)) {
-            scale = 0.0
+        DispatchQueue.main.asyncAfter(deadline: .now() + breathExercise.inBreathDuration + breathExercise.fullBreathHoldDuration) {
+            withAnimation(.easeInOut(duration: breathExercise.outBreathDuration)) {
+                scale = 0.0
+                queuVibrations(totalDuration: breathExercise.outBreathDuration)
+            }
         }
         
         DispatchQueue.main.asyncAfter(deadline: .now() + breathExercise.inBreathDuration + breathExercise.fullBreathHoldDuration + breathExercise.outBreathDuration + breathExercise.emptyHoldDuration) {
