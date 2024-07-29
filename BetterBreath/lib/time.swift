5064e5c6cea21102813c0e8548cf1f735ddb68cd
valtteri laine <valtteri@veristable.com>
2024-07-29 12:22:16 +0300

    fix: use truncatingReminder instead of reminder

--- BetterBreath/lib/time.swift
+++ BetterBreath/lib/time.swift
@@ -10,6 +10,6 @@ import Foundation
 
 func formatSeconds(seconds: Double) -> String {
     let minutes = seconds / 60
-    let remainingSeconds = seconds.remainder(dividingBy: 60)
+    let remainingSeconds = seconds.truncatingRemainder(dividingBy: 60)
     return String(format: "%02d:%02d", Int(minutes), Int(remainingSeconds))
 }
