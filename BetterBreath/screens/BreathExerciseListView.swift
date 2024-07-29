f519b3ef9e8edbc9f77c99f4d08fff002034e60b
valtteri laine <valtteri@veristable.com>
2024-07-29 08:08:31 +0300

    fix: stylise settings screen further

--- BetterBreath/screens/BreathExerciseListView.swift
+++ BetterBreath/screens/BreathExerciseListView.swift
@@ -21,10 +21,8 @@ struct BreathExerciseListView: View {
             List {
                 ForEach(breathExercises) { breathExercise in
                     NavigationLink(destination: BreathExerciseView(breathExercise: breathExercise)) {
-                        
                         VStack(alignment: .leading) {
                             Text("Breath exercise").bold()
-                            
                             Text("Duration: \(formatSeconds(seconds: breathExercise.exerciseDuration))")
                             Text("Pattern: \(String(format: "%.1f", breathExercise.inBreathDuration)) - \(String(format: "%.1f", breathExercise.fullBreathHoldDuration)) - \(String(format: "%.1f", breathExercise.outBreathDuration)) - \(String(format: "%.1f", breathExercise.emptyHoldDuration))")
                         }
--- BetterBreath/screens/BreathExerciseSettingsView.swift
+++ BetterBreath/screens/BreathExerciseSettingsView.swift
@@ -1,10 +1,22 @@
 import SwiftUI
 
+enum PickerType: String {
+    case inBreath = "Select In Breath Duration"
+    case fullBreathHold = "Select Full Breath Hold Duration"
+    case outBreath = "Select Out Breath Duration"
+    case emptyHold = "Select Empty Hold Breath Duration"
+    case exerciseDuration = "Select Exercise Duration"
+    
+    var title: String {
+        return self.rawValue
+    }
+}
+
 struct BreathExerciseSettingsView: View {
     
     @Environment(\.modelContext) private var modelContext
     @Environment(\.dismiss) var dismiss
-
+    
     @State private var inBreathDuration: Double = 5.0
     @State private var fullBreathHoldDuration: Double = 0.0
     @State private var outBreathDuration: Double = 5.0
@@ -12,59 +24,76 @@ struct BreathExerciseSettingsView: View {
     @State private var exerciseDuration: Double = 60.0
     @State private var pickerTitle: String = ""
     @State private var exerciseName: String = "New exercise"
-
+    
     // Separate state for each picker presentation
-    @State private var showInBreathPicker = false
-    @State private var showFullBreathHoldPicker = false
-    @State private var showOutBreathPicker = false
-    @State private var showEmptyHoldPicker = false
-    @State private var showExerciseDurationPicker = false
-
+    @State private var showPicker = false
+    @State private var pickerTimeInSeconds: Double = 0.0
+    
     var body: some View {
-        VStack {
-            durationButton("In Breath Duration", duration: $inBreathDuration, showPicker: $showInBreathPicker)
-            durationButton("Full Breath Hold Duration", duration: $fullBreathHoldDuration, showPicker: $showFullBreathHoldPicker)
-            durationButton("Out Breath Duration", duration: $outBreathDuration, showPicker: $showOutBreathPicker)
-            durationButton("Empty Hold Breath Duration", duration: $emptyHoldDuration, showPicker: $showEmptyHoldPicker)
-            durationButton("Exercise Duration", duration: $exerciseDuration, showPicker: $showExerciseDurationPicker)
-
-            Button(action: {
-                saveExercise()
-                dismiss()
-            }) {
-                Text("Save")
-                    .font(.headline)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.green)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-            }.padding()
+        List {
+            durationButton(PickerType.inBreath.rawValue, duration: $inBreathDuration)
+            durationButton(PickerType.fullBreathHold.rawValue, duration: $fullBreathHoldDuration)
+            durationButton(PickerType.outBreath.rawValue, duration: $outBreathDuration)
+            durationButton(PickerType.emptyHold.rawValue, duration: $emptyHoldDuration)
+            durationButton(PickerType.exerciseDuration.rawValue, duration: $exerciseDuration)
         }
-    }
-
-    private func durationButton(_ title: String, duration: Binding<Double>, showPicker: Binding<Bool>) -> some View {
+        .sheet(isPresented: $showPicker) {
+            TimePickerModalView(
+                selectedTimeInSeconds: $pickerTimeInSeconds,
+                title: $pickerTitle
+            ).onChange(of: pickerTimeInSeconds) { newValue in
+                switch pickerTitle {
+                case PickerType.inBreath.rawValue:
+                    inBreathDuration = newValue
+                case PickerType.fullBreathHold.rawValue:
+                    fullBreathHoldDuration = newValue
+                case PickerType.outBreath.rawValue:
+                    outBreathDuration = newValue
+                case PickerType.emptyHold.rawValue:
+                    emptyHoldDuration = newValue
+                case PickerType.exerciseDuration.rawValue:
+                    exerciseDuration = newValue
+                default:
+                    break
+                }
+            }
+        }
+        
         Button(action: {
-            pickerTitle = "Select \(title)"
-            showPicker.wrappedValue = true
+            saveExercise()
+            dismiss()
         }) {
-            Text("\(title): \(duration.wrappedValue, specifier: "%.1f") seconds")
+            Text("Save")
                 .font(.headline)
                 .padding()
                 .frame(maxWidth: .infinity)
-                .background(Color.blue)
+                .background(Color.green)
                 .foregroundColor(.white)
                 .cornerRadius(10)
-        }
-        .sheet(isPresented: showPicker) {
-            TimePickerModalView(
-                selectedTimeInSeconds: duration,
-                title: $pickerTitle
-            )
-        }
-        .padding()
+        }.padding()
     }
-
+    
+    private func durationButton(_ title: String, duration: Binding<Double>) -> some View {
+        Button(
+            action: {
+                pickerTitle = "\(title)"
+                showPicker = true
+                pickerTimeInSeconds = duration.wrappedValue
+            }) {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("\(duration.wrappedValue, specifier: "%.1f") sec").bold()
+                        Text("\(title)")
+                    }
+                    Spacer()
+                    Image(systemName: "chevron.right") // Example system icon
+                        .imageScale(.small)
+                        .foregroundColor(.gray)
+                }
+            }
+            .padding()
+    }
+    
     func saveExercise() {
         withAnimation {
             let newItem = BreathExercise(
