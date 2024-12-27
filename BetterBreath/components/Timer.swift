218f1ddc0dfef9ffec961ff3855e628ea0ff5523
Valtteri Laine <valtteri.laine@ouraring.com>
2024-12-27 13:04:22 +0200

    fix: timer

--- BetterBreath/components/Timer.swift
+++ BetterBreath/components/Timer.swift
@@ -4,21 +4,25 @@ struct TimerView: View {
     @Binding var timeElapsed: Double
     @Binding var isActive: Bool
 
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect() // Updates every second for simplicity
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+
+    @State private var startedAt: Date = .init()
 
     var body: some View {
-        Text(timeFormatted(timeElapsed))
-            .font(.title)
-            .onReceive(timer) { _ in
-                if !isActive {
-                    return
-                }
+        Text(
+            timeFormatted(timeElapsed)
+        )
+        .font(.title)
+        .onReceive(timer) { _ in
+            if !isActive {
+                return
+            }
 
-                timeElapsed += 1 // Increment the counter by 1 second
-            }
-            .onDisappear {
-                timer.upstream.connect().cancel() // Stop the timer when the view disappears
-            }
+            timeElapsed = startedAt.distance(to: Date.now)
+        }
+        .onDisappear {
+            timer.upstream.connect().cancel()
+        }
     }
 
     func timeFormatted(_ totalSeconds: Double) -> String {
